0f036815d93ddebc55708b8a11495741007f1d46
ErikBoesen <me@erikboesen.com>
2016-12-21 22:16:05 -0500

    Some static page basics

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+    get 'home' => 'static_pages#home'
+    get 'about' => 'static_pages#about'
+
     resources :microposts
     resources :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    root 'application#home'
+    root 'static_pages#home'
 
 end
