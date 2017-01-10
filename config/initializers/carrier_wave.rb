122db1cd6e3dba793c8d9953e524e5814a209361
ErikBoesen <erik.boesen@gmail.com>
2017-01-10 15:12:13 -0500

    Temporarily disable images in production

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,4 @@
-if Rails.env.production?
+"""if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
@@ -8,4 +8,4 @@ if Rails.env.production?
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
-end
+end"""
