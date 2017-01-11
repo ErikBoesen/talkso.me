fe9b26e86f910674db43d0a55a0a52324cfb8cab
ErikBoesen <erik.boesen@gmail.com>
2017-01-10 20:31:05 -0500

    Fix uncompiled assets in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -68,8 +68,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :username, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
     end
 
     # Before filters
