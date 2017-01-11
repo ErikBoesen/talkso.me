3e6ba8e402af951a0644be33d0d273e8721603a7
ErikBoesen <erik.boesen@gmail.com>
2017-01-11 11:01:59 -0500

    Fix issue with redirecting to broken URL on signin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'talkso.me',
     :enable_starttls_auto => true
   }
 
